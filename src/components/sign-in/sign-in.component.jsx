9d1a90d479c025abc20042511ca9a395b202ed0b
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-02-08 09:38:56 +0700

    + signin and form input component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './sign-in.styles.scss'
+import FormInput from '../form-input/form-input.component'
+
+class Signin extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({email: '', password: ''})
+
+    }
+
+    handleChange = (event) => {
+        const {value, name} = event.target
+        this.setState({[name]: value})
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have account</h2>
+                <span>Signin with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        type="email" 
+                        name="email"
+                        handleChange={this.handleChange} 
+                        value={this.state.email} 
+                        label="Email"
+                        required/>
+                    <FormInput 
+                        type="password" 
+                        name="password"
+                        value={this.state.password} 
+                        handleChange={this.handleChange} 
+                        label="Password"
+                        required/>
+                    <input type="submit" value="Submit Form"/>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default Signin
