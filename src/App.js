cd3c73420bd29607c0574efc51eb9510f6d54332
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-02-08 09:38:34 +0700

    + auth page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Auth from './pages/auth/auth.component'
 import Header from './components/header/header.component'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 	  <Switch>
     	<Route exact path='/' component={HomePage}></Route>
     	<Route path='/shop' component={ShopPage}></Route>
+    	<Route path='/signin' component={Auth}></Route>
 	  </Switch>
   </div>
 }
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './auth.styles.scss'
+import Signin from '../../components/sign-in/sign-in.component'
+
+const Auth = () => (
+    <div className="sign-in-and-sign-up">
+        <Signin />
+    </div>
+)
+
+export default Auth
