fdaaacffd4f48721031db2b0ed68e2ae24482a82
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-02-12 05:37:45 +0700

    ^ signup with email and password

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 
-import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
+import { auth, createUserProfilDocument } from '../../firebase/firebase.utils'
 
 import './sign-up.styles.scss'
 
@@ -29,8 +29,8 @@ class SignUp extends React.Component {
         }
 
         try{
-            const {user} = await createUserWithEmailAndPassword(email, password)
-            await createUserProfileDocument(user, {displayName})
+            const {user} = await auth.createUserWithEmailAndPassword(email, password)
+            await createUserProfilDocument(user, {displayName})
             this.setState({
                 displayName: '',
                 email: '',
--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './auth.styles.scss'
 import Signin from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 const Auth = () => (
     <div className="sign-in-and-sign-up">
         <Signin />
+        <SignUp />
     </div>
 )
 
