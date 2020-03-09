b2a1d746c306c34be055c7d28f8609f814f6c665
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-03-10 04:58:02 +0700

    ^ nested route shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import CollectionOverview from '../../components/collection-overview/collection-overview.component'
+import CategoryPage from '../category/category.component'
 
-const ShopPage = () =>  (
-        <div className="shop-page">
-            <CollectionOverview />
-        </div>
+const ShopPage = ({match}) =>  (
+    <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+    </div>
 )
 
 
