2bb6a42cd227b6981e5de50caca08329dab36fce
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-03-28 13:24:45 +0700

    ^ update shop data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,6 @@
 import {createSelector} from 'reselect'
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5,
-}
+
 const selectShop = state => state.shop
 
 export const selectCollections = createSelector(
@@ -14,9 +8,14 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
 export const selectCollection = collectionUrlParams => (
     createSelector(
         [selectCollections],
-        collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParams])
+        collections => collections[collectionUrlParams]
     )
 )
